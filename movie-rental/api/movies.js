46046b84e48cdb87890e937cae472d26fc01ec9b
Victoria Bock <thenameisjude@Victorias-Mac-mini.local>
2025-03-13 18:37:17 -0700

    setting up db and beginning of api

---
+++ movie-rental/api/movies.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const db = require('..db');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try{
+        const result = await db.query('SELECT * FROM movies');
+        res.json(result.rows);
+    }catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/', async (req,res) => {
+    const { title, genre, director, release_year, rental_price } = req.body;
+    const query = `
+    INSERT INTO movies (title, genre, director, release_year, rental_price)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;
+    `;
+    const values = [title, genre, director, release_year, rental_price];
+
+    try{
+        const result = await db.query(query, values);
+        res.status(201).json(result.rows[0]);
+    }catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+module.exports = router;
